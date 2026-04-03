9fc835360c6617ea60303b2ec6eca2c26337d756
develper21 <githubnarvin@gmail.com>
2026-04-03 21:49:24 +0530

    chore(router): add v7 future flags to suppress warnings

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import AppRoutes from './routes/AppRoutes'
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <AppRoutes />
       </Router>
     </AuthProvider>
